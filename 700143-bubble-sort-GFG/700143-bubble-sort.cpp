287692b731b417d4aead84e2b2ccf7f533510dc8
Priyanshu00102 <kumaramansr24@gmail.com>
2026-06-23 21:41:51 +0530

    Added solution - LeetHub

---
+++ 700143-bubble-sort-GFG/700143-bubble-sort.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        int n = arr.size();
+        for(int i = n - 1; i >= 1; i--) {
+            for(int j = 0; j < i; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    swap(arr[j], arr[j + 1]);
+                }
+            }
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
