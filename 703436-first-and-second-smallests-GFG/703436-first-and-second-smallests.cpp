62a0b0358e0a7d357deeeb530a0e09d1704b7509
Priyanshu00102 <kumaramansr24@gmail.com>
2026-07-04 20:00:22 +0530

    Added solution - LeetHub

--- 703436-first-and-second-smallests-GFG/703436-first-and-second-smallests.cpp
+++ 703436-first-and-second-smallests-GFG/703436-first-and-second-smallests.cpp
@@ -1,27 +1,13 @@
 class Solution {
   public:
- vector<int> secondsmallest(vector<int> &arr, int n)
-  {
-      int small=arr[0], sec_small=INT_MAX;
-      for(int i=1; i<n; i++){
-          if(arr[i]<small)
-          {
-              sec_small=small;
-              small=arr[i];
-          }
-          else if(arr[i]<sec_small && arr[i]!=small)
-          sec_small=arr[i];
-      }
-      if(sec_small == INT_MAX)
-            {
-             return {-1};
-            }
-      return {small, sec_small};
-     
-  }
     vector<int> minAnd2ndMin(vector<int> &arr) {
-        
-        return secondsmallest(arr, arr.size());
+        // code here
+        sort(arr.begin(),arr.end());
+      for(int i=0; i<arr.size(); i++){
+          if(arr[0]!=arr[i]){
+              return {arr[0],arr[i]};
+          }
+      } return {-1};
     }
 };
 
