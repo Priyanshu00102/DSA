c56e035f16425867508520b7e79f18694ddc021b
Priyanshu00102 <kumaramansr24@gmail.com>
2026-07-10 12:36:48 +0530

    Added solution - LeetHub

---
+++ 701210-array-leaders-GFG/701210-array-leaders.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> leaders(vector<int>& arr) {
+        vector<int> v;
+        int n = arr.size();
+
+        int maxi = arr[n - 1];
+        v.push_back(maxi);
+
+        for(int i = n - 2; i >= 0; i--) {
+            if(arr[i] >= maxi) {   // >= because equal is also a leader
+                maxi = arr[i];
+                v.push_back(arr[i]);
+            }
+        }
+
+        reverse(v.begin(), v.end());
+        return v;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
